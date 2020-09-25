2a74c07231f478963c9b68eb5bc7bf7bd213eab7
tom.hooijenga <tom.hooijenga@myparcel.nl>
2020-09-25 08:56:00 +0200

    Add tests

---
+++ test/validation.js
@@ -0,0 +1,28 @@
+const postalCodes = require('../src/index');
+const expect = require('chai').expect;
+
+describe('Postal codes validation: ', function () {
+
+  const countries = require('../data/countries.json');
+  const tests = require('../data/tests.json');
+
+  Object.values(countries).forEach(function (country) {
+    if (country.postalCodeFormat === false) {
+      return;
+    }
+
+    const data = tests[country.postalCodeFormat];
+
+    data.valid.forEach(function (validPostalCode) {
+      it(country.alpha2 + ' / ' + validPostalCode + ' is valid', function () {
+        expect(postalCodes.validate(country.alpha2, validPostalCode)).to.be.true;
+      })
+    });
+
+    data.invalid.forEach(function (invalidPostalCode) {
+      it(country.alpha2 + ' / ' + invalidPostalCode + ' is valid', function () {
+        expect(postalCodes.validate(country.alpha2, invalidPostalCode)).to.be.false;
+      })
+    });
+  });
+});
