c47b01d700afe3ca7a5f89c9be938ccd2d1fc8c7
tom.hooijenga <tom.hooijenga@myparcel.nl>
2020-09-25 12:20:08 +0200

    Use arrow functions

--- test/args.js
+++ test/args.js
@@ -1,36 +1,36 @@
 const postalCodes = require('../src/index');
 const expect = require('chai').expect;
 
-describe('Argument validation:', function () {
-  it('throws when args are missing', function () {
-    expect(function () {
+describe('Argument validation:', () => {
+  it('throws when args are missing', () => {
+    expect(() => {
       postalCodes.validate(undefined, '1234ab');
     }).to.throw('Missing country code.');
 
-    expect(function () {
+    expect(() => {
       postalCodes.validate('NL', undefined);
     }).to.throw('Missing postal code.');
   });
 
-  it('throws for invalid countries', function () {
-    expect(function () {
+  it('throws for invalid countries', () => {
+    expect(() => {
       postalCodes.validate('nope', '1234ab');
     }).to.throw('No data for [nope].');
   });
 
-  it('should accept different keys', function () {
+  it('should accept different keys', () => {
     expect(postalCodes.validate('NL', '1234ab')).to.be.true;
     expect(postalCodes.validate('NLD', '1234ab')).to.be.true;
     expect(postalCodes.validate('528', '1234ab')).to.be.true;
   });
 
-  it('should normalize keys', function () {
+  it('should normalize keys', () => {
     expect(postalCodes.validate('  NL  ', '1234ab')).to.be.true;
     expect(postalCodes.validate('nlD', '1234ab')).to.be.true;
     expect(postalCodes.validate(528, '1234ab')).to.be.true;
   });
 
-  it('should normalize the postal codes', function () {
+  it('should normalize the postal codes', () => {
     expect(postalCodes.validate('NL', ' 1234-ab ')).to.be.true;
     expect(postalCodes.validate('NOR', 1234)).to.be.true;
   });
--- test/validation.js
+++ test/validation.js
@@ -1,14 +1,13 @@
 const postalCodes = require('../src/index');
 const expect = require('chai').expect;
+const countries = require('../data/countries.json');
+const tests = require('../data/tests.json');
 
-describe('Postal codes validation: ', function () {
+describe('Postal codes validation: ', () => {
 
-  const countries = require('../data/countries.json');
-  const tests = require('../data/tests.json');
-
-  Object.values(countries).forEach(function (country) {
+  Object.values(countries).forEach((country) => {
     if (country.postalCodeFormat === false) {
-      it(country.alpha2 + ' is valid', function () {
+      it(country.alpha2 + ' is valid', () => {
         expect(postalCodes.validate(country.alpha2, 'abc')).to.be.true;
       });
       return;
@@ -17,13 +16,13 @@ describe('Postal codes validation: ', function () {
     const data = tests[country.postalCodeFormat];
 
     data.valid.forEach(function (validPostalCode) {
-      it(country.alpha2 + ' / ' + validPostalCode + ' is valid', function () {
+      it(country.alpha2 + ' / ' + validPostalCode + ' is valid', () => {
         expect(postalCodes.validate(country.alpha2, validPostalCode)).to.be.true;
       })
     });
 
     data.invalid.forEach(function (invalidPostalCode) {
-      it(country.alpha2 + ' / ' + invalidPostalCode + ' is valid', function () {
+      it(country.alpha2 + ' / ' + invalidPostalCode + ' is valid', () => {
         expect(postalCodes.validate(country.alpha2, invalidPostalCode)).to.be.false;
       })
     });
